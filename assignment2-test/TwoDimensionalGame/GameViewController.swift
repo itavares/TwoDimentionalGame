86ffe76de2aa959d074fd7317ae6dbfba99cb39d
itavares <f.tavares@wustl.edu>
2018-07-05 17:03:25 -0500

    middle icon added

--- assignment2-test/TwoDimensionalGame/GameViewController.swift
+++ assignment2-test/TwoDimensionalGame/GameViewController.swift
@@ -2,12 +2,14 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
     //labels that for x and y coordinates
     @IBOutlet weak var xCoordinate: UILabel!
     @IBOutlet weak var yCoordinate: UILabel!
     @IBOutlet weak var eventLogUpdate: UILabel!
     @IBOutlet weak var eventSupriseLabel: UILabel!
+    
+    @IBOutlet weak var mapLocationImageView: UIImageView!
     
     //button labels to enable and disable
     @IBOutlet weak var northControlButton: UIButton!
@@ -23,14 +25,17 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         eventLogUpdate.isEnabled = false
-//        gameModel.move(direction: .east)
-//        gameModel.move(direction: .east)
-//        print(gameModel.currentLocation.x)
-//        gameModel.restart()
-//        print(gameModel.currentLocation)
+        mapLocationImageView.frame = CGRect(x: 170, y: 390
+            , width: 40 , height: 35)
         
     }
+    // global variables for dot movement !
+    var yPosLabel = 390
+    var xPosLabel = 170
+    var newPos = CGRect(x: 170, y: 390
+        , width: 40 , height: 35)
     
+    let imageTypes = ["mapDot","mapDotAlert"]
     
     @IBAction func northButton(_ sender: UIButton) {
         gameModel.move(direction: .north)
@@ -44,7 +49,13 @@ class GameViewController: UIViewController {
         else if (gameModel.currentLocation.y != -2){
             southControlButton.isEnabled = true
         }
-    
+        //        UIView.animate(withDuration: 1.5, animations: ({self.eventLogUpdate.frame = newPos }), completion: nil)
+        yPosLabel -= 20 //move down on y axis
+        newPos = CGRect(x: xPosLabel, y: yPosLabel
+            , width: 40 , height: 35)
+        self.mapLocationImageView.frame = newPos
+//        self.mapLocationImageView.image = UIImage(named: imageTypes[1])
+        checkSecretEvent()
     }
     
     @IBAction func eastButton(_ sender: UIButton) {
@@ -59,8 +70,11 @@ class GameViewController: UIViewController {
         else if (gameModel.currentLocation.x != -2){
             westControlButton.isEnabled = true
         }
-        
-        
+        xPosLabel += 20 //move down on y axis
+        newPos = CGRect(x: xPosLabel, y: yPosLabel
+            , width: 40 , height: 35)
+        self.mapLocationImageView.frame = newPos
+        checkSecretEvent()
     }
     
     @IBAction func southButton(_ sender: UIButton) {
@@ -76,7 +90,11 @@ class GameViewController: UIViewController {
         else if (gameModel.currentLocation.y != 2){
             northControlButton.isEnabled = true
         }
-     
+        yPosLabel += 20 //move down on y axis
+        newPos = CGRect(x: xPosLabel, y: yPosLabel
+            , width: 40 , height: 35)
+        self.mapLocationImageView.frame = newPos
+        checkSecretEvent()
     }
     
     @IBAction func westButton(_ sender: UIButton) {
@@ -91,7 +109,11 @@ class GameViewController: UIViewController {
         else if (gameModel.currentLocation.x != 2){
             eastControlButton.isEnabled = true
         }
-      
+        xPosLabel -= 20 //move down on y axis
+        newPos = CGRect(x: xPosLabel, y: yPosLabel
+            , width: 40 , height: 35)
+        self.mapLocationImageView.frame = newPos
+        checkSecretEvent()
     }
     
     @IBAction func resetButton(_ sender: UIButton) {
@@ -107,8 +129,20 @@ class GameViewController: UIViewController {
         westControlButton.isEnabled = true
         
         eventLogUpdate.text = ""
-        eventSupriseLabel.text = "" 
+        eventSupriseLabel.text = ""
+        self.mapLocationImageView.frame = CGRect(x: 170, y: 390
+            , width: 40 , height: 35)
+        self.mapLocationImageView.image = UIImage(named: imageTypes[0])
         
+    }
+    // helper fuction to check for secre messages and change image
+    func checkSecretEvent (){
+        if yPosLabel == 370 && xPosLabel == 150 {
+            self.mapLocationImageView.image = UIImage(named: imageTypes[1])
+        }
+        else{
+            self.mapLocationImageView.image = UIImage(named: imageTypes[0])
+        }
     }
     
     
